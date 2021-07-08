2faf1038af1eecb9f66b6ddb8675a4e66f4ccebc
Tamir Montalto <tmontalto@MacBook-Pro-2.fios-router.home>
2021-07-08 19:26:02 -0400

    added items component

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,14 +1,12 @@
 import React from 'react'
+import Item from './Item'
 
 export default function HomePage() {
     return (
         <div>
             <h1>Home Page</h1>
             <div>
-                <h3>Item Name</h3>
-                <p>$ price</p>
-                <input type="checkbox" id="like" name="like"/>
-                <label for="like">Like</label>
+                < Item />
             </div>
         </div>
     )
